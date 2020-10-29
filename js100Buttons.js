f6da0311c88dba32c5e7ef0342df1ea97b2baa6c
Julia Ballo <ballo.julia@gmail.com>
2020-10-28 19:49:43 -0700

    First Commit

---
+++ js100Buttons.js
@@ -0,0 +1,6 @@
+// WRITE YOUR CODE IN HERE:
+for (let i = 0; i < 100; i++) {
+    const button = document.createElement("button");
+    button.innerHTML = "Button";        
+    document.querySelector('#container').appendChild(button);
+}
